50b8f23b051d75d9f7ebea4ad791cfec462f92d6
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-19 01:58:44 +0000

    I made a program to calculate the perimeter of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Date: 09.18.22
+# This program alculates the area and perimeter of a rectangle
+#with the dimension 9x3
+
+
+def main():
+   #this function alculates the area and perimeter of a rectangle.
+   print("If a rectangle has the dimensions:")
+   print("9 cm x 3cm")
+   print("")
+   print("Area is {} cm^2".format(9*3))
+   print("Area is {} cm".format((9+3)*2))
+  
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Date: 09-18-22
+# This program does basic math.
+
+
+def main():
+    #This function does basic math.
+    print(9+2)
+    print("7+2={}".format(7+2))
+    print("4+2/2={}".format(4+2/2))
+    print("8+2*2={}".format(8+2*2))
+    print("1+3^2={}".format(1+3^2))
+    print("1+3**2={}".format(1+3**2))
+    print(56%5)
+if __name__=="__main__":
+    main()
